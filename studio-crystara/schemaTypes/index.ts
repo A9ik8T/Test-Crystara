e56ab90f931248cf4561a5c9ec3f8b4b6a989230
Aniket Nandi <anketandi@gmail.com>
2026-03-22 21:59:09 +0530

    Add files via upload

---
+++ studio-crystara/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import { productCategory } from './productCategory'
+import { productSubCategory } from './productSubCategory'
+import { product } from './product'
+
+export const schemaTypes = [productCategory, productSubCategory, product]
